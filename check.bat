7b5a2b49afe7d030e48003b6a561b939e34d2518
wyattscarpenter <wyattscarpenter@gmail.com>
2024-08-29 07:14:36 -0700

    since there's now an on-purpose trailing whitespace in this project, I must use only git diff --check instead of (util's) git check

--- check.bat
+++ check.bat
@@ -1,1 +1,1 @@
-test.bat && ruff check --ignore E741 && pyright && mypy --install-types --pretty . && git diff --check && git check
+test.bat && ruff check --ignore E741 && pyright && mypy --install-types --pretty . && git diff --check
