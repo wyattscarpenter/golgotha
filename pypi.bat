ff2c5f70bceba65f72bf6453545282558e9d9262
wyattscarpenter <wyattscarpenter@gmail.com>
2026-08-16 20:51:57 -0700

    a note in the pypi script

--- pypi.bat
+++ pypi.bat
@@ -1,1 +1,2 @@
 check.bat && pip install -U build twine && python -m build && twine upload --skip-existing dist/*
+REM See also git-pip-publish in gyatt, which is like basically this script but without the check.bat invocation so not as good for this particular project: https://github.com/wyattscarpenter/gyatt/blob/master/git-pip-publish
